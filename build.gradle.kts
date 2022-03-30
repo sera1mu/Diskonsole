164a00f1461aa8cff5164663f5aece940862ea28
Seraimu <seraimu.neko@outlook.jp>
2022-03-30 12:58:59 +0900

    chore(*): Init Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "me.seraimu"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
